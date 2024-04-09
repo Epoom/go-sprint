dfb5b3dad6252f6b8c60844182d3d1b2a3b2d3e8
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-09 15:51:48 +0300

    l

--- shift_by.go
+++ shift_by.go
@@ -1,6 +1,9 @@
 package sprint
 
-func ShiftBy(r rune, step int) {
-	shift := r + rune(step)
-	return shift
+func ShiftBy(r rune, step int) rune {
+	if r < 'a' || r > 'z' {
+		return r
+	}
+
+	return ((r - 'a' + rune(step)) % 26) + 'a'
 } 
