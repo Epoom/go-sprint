973471a1954cd15b6fa7dc65e1682040f2296023
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-10 13:42:54 +0300

    woo

--- is_leap_year.go
+++ is_leap_year.go
@@ -2,10 +2,9 @@ package sprint
 
 func IsLeapYear(year int) bool {
 	if year % 4 == 0 {
-		return bool(true)
-	}
-	if year % 400 == 0 {
+		if year % 400 == 0 {
 			return bool(true)
+			}
 	}
 	return bool(false)
 }
