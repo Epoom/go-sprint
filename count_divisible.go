0cc52681ed39a15d5805916897a8d48177e1c3a0
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-10 14:14:04 +0300

    yippedoo

---
+++ count_divisible.go
@@ -0,0 +1,17 @@
+package sprint
+
+func CountDivisible(from, to, step, divisor int) int {
+	if step <= 0 {
+		return 0
+	}
+	if divisor == 0 {
+		return 0
+	}
+	count := 0
+	for i := from ; i < to; i =+ step {
+		if i % divisor == 0 {
+			count = count + 1
+		}
+		return count
+	} 
+}
