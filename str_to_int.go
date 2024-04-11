e1a0d3e775f6603cf737af19afe989b507db52ce
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-11 16:44:38 +0300

    pwease wowk, then my pwevious c knowedge will have been usewul

---
+++ str_to_int.go
@@ -0,0 +1,23 @@
+package sprint
+
+func StrToInt(s string) int {
+	x := 1
+	nb := 0
+	i := 0
+
+	if s[i] == ' ' {
+		return 0
+	}
+	for s[i] == '-' || s[i] == '+' {
+		if s[i] == '-' {
+			x *= -1
+		}
+		i++
+	}
+	for s[i] >= '0' || s[i] <= '9' {
+		nb = (nb * 10) + (int(s[i] - '0'))
+		i++
+	}
+	nb *= x
+	return nb
+}
