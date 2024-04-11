c4b45444852fbb7addc5e6ca73f18037601ad5e3
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-11 11:20:34 +0300

    please

--- pairs.go
+++ pairs.go
@@ -7,12 +7,12 @@ func Pairs() string {
 for i := 0; i <= 99; i++ {
 	for j := i + 1; j <= 99; j++ {
 		str += fmt.Sprintf("%02d %02d, ", i, j)
-			if i == 98 && j == 99 {
-				str += fmt.Sprintf("%02d %02d", i, j)
-		}
 	}
-
 	}
-	return str
+	newstr := ""
+	if len(str) > 0 {
+		newstr = str[:len(str)-2]
+	}
+	return newstr
 }
 
