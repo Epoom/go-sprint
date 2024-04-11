6804fa89ae60144becea2d5fbe6a9f289d0d75d2
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-11 10:35:09 +0300

    hope

--- pairs.go
+++ pairs.go
@@ -5,9 +5,11 @@ import "fmt"
 func Pairs() string {
 	str := ""
 for i := 0; i <= 99; i++ {
-	for j := 0; j <= 99; j++ {
+	for j := i + 1; j <= 99; j++ {
 		str += fmt.Sprintf("%02d %02d, ", i, j)
-		}
 	}
+	}
+
+
 	return str
 }
