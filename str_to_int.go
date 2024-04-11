22d7fd9006e021b0e739094d17f1e4dd396846a0
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-11 17:09:44 +0300

    pwease wowk, then my pwevious c knowedge will have been usewul

--- str_to_int.go
+++ str_to_int.go
@@ -5,17 +5,20 @@ func StrToInt(s string) int {
 	nb := 0
 	i := 0
 
-	if len(s) == 0 || s[i] == ' ' {
+	if len(s) == 0 {
 		return 0
 	}
-	if s[i] == '-' && s[i] +1 == '-' || s[i] == '+' && s[i] +1 == '+' {
-		return 0
-	}
+
 	for s[i] == '-' || s[i] == '+' {
 		if s[i] == '-' {
 			x *= -1
 		}
 		i++
+
+	if s[i] == '-' || s[i] == '+' {
+		return 0
+	}
+
 	}
 	for i < len(s) && s[i] >= '0' && s[i] <= '9' {
 		nb = (nb * 10) + (int(s[i] - '0'))
