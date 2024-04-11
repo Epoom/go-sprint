004d5c16b141cc59d7614ef30b157f10d8bc7136
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-11 13:23:30 +0300

    uwu

--- countdown.go
+++ countdown.go
@@ -1,13 +1,15 @@
-package sprint
+package main
 
-import "fmt"
+import (
+	"strconv"
+)
 
 func Countdown(n int) string {
 	str := ""
 	for i := n; i > 0; i -= 2 {
-		str += fmt.Sprintf("%d, ", i)
+		str += strconv.Itoa(i) + ", "
 	}
 	newstr := ""
 	newstr = str + "0!"
-	return newstr
+	return string(newstr)
 }
