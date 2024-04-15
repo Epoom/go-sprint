2061ca38dbe2bb62f625c8096f7fb107c5eea51e
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-15 10:23:20 +0300

    y

--- generate_range.go
+++ generate_range.go
@@ -2,10 +2,10 @@ package sprint
 
 func GenerateRange(min, max int) []int {
 
-lenght := max - min + 1
+length := max - min
 var a []int
 
-for i := 0; i < lenght; i++ {
+for i := 0; i < length; i++ {
 	a = append(a, min+i)
 	}
 	return a
