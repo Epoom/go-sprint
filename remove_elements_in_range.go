c55987b28cd79c4789331eb9b5c2248a901a5286
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-15 11:16:36 +0300

    yippee

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -8,20 +8,21 @@ func RemoveElementsInRange(arr []float64, from, to int) []float64 {
 		from += len(arr)
 	}
 	from = from % len(arr)
+
 	for to < 0 {
 		to += len(arr)
 	}
+	to = to % len(arr)
 
-	to = to% len(arr)
 	if from > to {
 		from, to = to, from
 	}
-arr = append(arr[:from], arr[to:]...)
-	if arr == nil {
+	result := append(arr[:from], arr[to:]...)
+	if result == nil {
 		return []float64{}
 	}
 
-	return arr
+	return result
 }
 
 /*func main() {
