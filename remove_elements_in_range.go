9cd9f407718974ebcb23936efcd70483837d87c5
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-15 10:24:22 +0300

    y

---
+++ remove_elements_in_range.go
@@ -0,0 +1,35 @@
+package sprint /*main*/
+
+/*import "fmt"*/
+
+func removeElementsInRange(arr []float64, from, to int) []float64 {
+	for from < 0 || from > len(arr) {
+		if from < 0 {
+			from += len(arr)
+		}
+		if from > len(arr) {
+			from -= len(arr)
+		}
+	}
+	for to < 0 || to > len(arr) {
+		if to < 0 {
+			to += len(arr)
+		}
+		if to > len(arr) {
+			to -= len(arr)
+		}
+	}
+	if from > to {
+		from, to = to, from
+	}
+	return append(arr[:from], arr[to:]...)
+}
+
+/*func main() {
+	input := []float64{10., .8, -.4, 20., 7.7, 3.}
+	from := 1
+	to := -36
+
+	result := removeElementsInRange(input, from, to)
+	fmt.Println(result) // Output: [10 7.7 3]
+}*/
