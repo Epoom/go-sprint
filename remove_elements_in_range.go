77ff4a3377d88d5ec4e0c001da90f0eda8d5fa3f
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-15 11:44:07 +0300

    yippee

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -3,33 +3,33 @@ package sprint /*main*/
 /*import "fmt"*/
 
 func RemoveElementsInRange(arr []float64, from, to int) []float64 {
-	length := len(arr)
+    length := len(arr)
 
+    // Normalize indices to fall within the slice bounds
+    for from < 0 {
+        from += length
+    }
+    for to < 0 {
+        to += length
+    }
 
-		for from < 0 {
-			from += length
-		}
-		for to < 0 {
-			to += length
-		}
+    for from >= length {
+        from -= length
+    }
+    for to >= length {
+        to -= length
+    }
 
-		if from >= length {
-			from -= length
-		}
-		if to >= lenght {
-			to -= length
-		}
-	
-	if from > to {
-		from, to = to, from
-	}
-	result := append(arr[:from], arr[to:]...)
-	
-	if len(result) == 0 {
-		return []float64{}
-	}
+    if from > to {
+        from, to = to, from
+    }
+    result := append(arr[:from], arr[to:]...)
+    
+    if len(result) == 0 {
+        return []float64{}
+    }
 
-	return result
+    return result
 }
 
 /*func main() {
