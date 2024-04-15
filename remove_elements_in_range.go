a39abeee395b2ce84930f8edae900ceeee7c6065
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-15 13:55:24 +0300

    ldjasoipd

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -3,7 +3,7 @@ package sprint /*main*/
 /*import "fmt"*/
 
 func RemoveElementsInRange(arr []float64, from, to int) []float64 {
-
+	
 	for from < 0 || from > len(arr) {
 		if from < 0 {
 			from += len(arr)
@@ -17,16 +17,16 @@ func RemoveElementsInRange(arr []float64, from, to int) []float64 {
 			to += len(arr)
 		}
 		if to >= len(arr) {
-			to = len(arr)
+			to = len(arr) - 1
 		}
 	}
 	if from > to {
 		from, to = to, from
 	}
-	if to == len(arr)-1 {
+	if to == len(arr) {
 		return arr[:from]
 	}
-	result := append(arr[:from], arr[to:]...)
+	result := append(arr[:from], arr[to+1:]...)
 
 	return result
 }
