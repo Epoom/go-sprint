881fb25819c1dae29d9215fb8ee6504ee4954f57
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-16 15:38:41 +0300

    yay

---
+++ to_upper_case.go
@@ -0,0 +1,13 @@
+package sprint
+
+func ToUpperCase(s string) string {
+	var str string
+	str = ""
+	for _, c := range s {
+		if c > 'a' || c < 'z' {
+			c -= 32
+			str = str + string(rune(c))
+		}
+	}
+	return str
+}
