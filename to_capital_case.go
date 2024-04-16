3e349afc654922b190c3f835c6c350079f88f9ef
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-16 16:43:11 +0300

    lol

--- to_capital_case.go
+++ to_capital_case.go
@@ -7,10 +7,7 @@ capitalize := true
 var str string
 
 for _, i := range s {
-	if i < 'A' || i > 'z' || (i < 0 && i > 9) {
-		capitalize = true
-		str += string(i)
-	} else if i >= 'A' && i <= 'Z' {
+	 if i >= 'A' && i <= 'Z' {
 		if !capitalize {
 			str += string(i + 32) 
 		} else {
@@ -23,6 +20,9 @@ for _, i := range s {
 			str += string(i)
 		}
 		capitalize = false
+	} else if i < 'A' || i > 'z' || (i < 0 && i > 9) {
+		capitalize = true
+		str += string(i)
 	} else {
 		str += string(i)
 	}
