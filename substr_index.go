f3958e9e024b619b75ff4f01e731c705bb5bc411
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-17 10:49:59 +0300

    w

--- substr_index.go
+++ substr_index.go
@@ -2,7 +2,7 @@ package sprint
 
 func SubstrIndex(s string, toFind string) int {
 
-for i := 0; i <= len(toFind); i++ {
+for i := 0; i <= len(s) - len(toFind); i++ {
 	match := true
 	for j := 0; j < len(toFind); j++ {
 		if s[i + j] != toFind[j] {
