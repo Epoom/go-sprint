cbda8cb9c15e4da93b77467bc9a256ce647fd2d6
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-18 11:36:10 +0300

    yippee

---
+++ point_diff.go
@@ -0,0 +1,20 @@
+package sprint
+
+type Point1 struct {
+	X float32
+	Y float32
+}
+
+func PointDiff(p1, p2 Point) Point {
+
+	coord1 := p1.X * p1.X + p1.Y * p1.Y
+	coord2 := p2.X * p2.X + p2.Y * p2.Y
+
+	if coord1 >= coord2 {
+		return p1
+	}
+	return p2
+}
+
+
+
