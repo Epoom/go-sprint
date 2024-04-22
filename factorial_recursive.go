e3fac575686b660b7294e1ff328e269c43aa8370
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-22 12:53:39 +0300

    nokia2

--- factorial_recursive.go
+++ factorial_recursive.go
@@ -1,8 +1,10 @@
 package sprint
 
 func FactorialRecursive(n int) int {
-
-	if n <= 0 || n == 1 {
+	if n < 0 {
+		return 0
+	}
+	if n == 0 || n == 1 {
 		return 1
 	} else {
 		return n * FactorialRecursive(n -1)
