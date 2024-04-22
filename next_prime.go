3f07d5f24f66b2b19a6dc01dd012a03178961e45
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-22 13:15:02 +0300

    Next nokia prime

---
+++ next_prime.go
@@ -0,0 +1,30 @@
+package sprint
+
+func NextPrime(n int) int {
+
+	if n < 2 {
+		return 2
+	}
+	if IsPrime(n)
+	{
+		return n
+	}
+	next = n + 1
+	for !NextPrime(next) {
+		n++
+	}
+	return next
+}
+
+func IsPrime(n int) bool {
+
+	if n < 2 {
+		return false
+	}
+	for i := 2; i < 46340 && i * i <= n; i++ {
+		if n % i == 0 {
+			return false
+		}
+	}
+	return true
+}
