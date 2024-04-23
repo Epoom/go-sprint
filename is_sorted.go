4653bae31d2b7d454429ba2ebc441de539f2437d
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-23 12:38:37 +0300

    sorted nokia

--- is_sorted.go
+++ is_sorted.go
@@ -2,14 +2,12 @@ package sprint
 
 func IsSorted(f func(a, b string) int, arr []string) bool {
 	n := len(arr)
-    for i := 0; i < n-1; i++ {
+	ascending := f(arr[0], arr[1]) > 0
+    for i := 1; i < n-1; i++ {
         result := f(arr[i], arr[i+1])
-        if result > 0 {
+        if (ascending && result < 0) || !ascending && result > 0  {
             return false
         }
-		if result < 0 {
-			return false
-		}
     }
 	return true
 }
