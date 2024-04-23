199fed00aeed02adc24b74b3d33946edc6457357
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-23 11:51:33 +0300

    advanced nokia

--- advanced_sort_word_arr.go
+++ advanced_sort_word_arr.go
@@ -4,11 +4,12 @@ func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
 	n := len(a)
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
-			if !(a[j], a[j+1]) {
+			if f(a[j], a[j+1]) > 0 {
 				a[j], a[j+1] = a[j+1], a[j]
 			}
 		}
 	}
+	return a
 }
 
 func StrCompare(a, b string) int {
