4271229527098ba452b6f8f78236026bc18ad7bb
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-24 13:45:18 +0300

    sort nokia

---
+++ sort_word_arr.go
@@ -0,0 +1,13 @@
+package sprint
+
+func SortWordArr(a []string) []string {
+
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
+	return a
+}
