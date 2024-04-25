d357367340f4aff2280738e8326d30915303cd1e
EGERTYAKOPOOM <egertyako.poom@gmail.com>
2024-04-25 15:36:34 +0300

    balanced nokia

---
+++ balanced_parentheses.go
@@ -0,0 +1,24 @@
+package sprint
+
+func BalancedParentheses(input string) bool {
+	stack := []rune{}
+
+	parentheses := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, c := range input {
+		switch c {
+			case '(', '{', '[':
+				stack = append(stack, c)
+			case ')', '}', ']':
+				if len(stack) == 0 || stack[len(stack)-1] != parentheses[char] {
+					return false
+				}
+				stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
